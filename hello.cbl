082fa045d14f24f9ea2f7962a99599923a1a1790
Robert Crummett <rncrummett5@gmail.com>
2025-02-23 08:04:25 -0700

    Lets git it

---
+++ hello.cbl
@@ -0,0 +1,10 @@
+      *> identification division
+       IDENTIFICATION DIVISION.
+      *> setup program id
+       PROGRAM-ID. HELLO.
+      *> setup procedure division
+       PROCEDURE DIVISION.
+      *> print a string
+           DISPLAY 'WELCOME'.
+      *> end the program
+       STOP RUN.
